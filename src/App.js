8f6c88ee368ae9045fcea88892c14f0994ebaa1a
chris-malloy <chrismalloymusic@gmail.com>
2017-11-30 15:37:02 -0500

    made Board component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Custom Components
+import Board from './Board';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="col-sm-12 text-center">
 				<h1>Welcome to Reaxas Holdem'</h1>
+				<Board />
 			</div>
 		);
 	}
---
+++ src/Board.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Board.css';
+
+class Board extends Component{
+    render(){
+        return(
+            <div id="board"></div>
+        )
+    }
+}
+
+export default Board;
